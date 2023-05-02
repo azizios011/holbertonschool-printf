2bd0319b13ae1799a7991210a8b641e7c422866d
azizios011 <azizhidri001@gmail.com>
2023-05-02 02:21:03 -1100

    fix

--- main.h
+++ main.h
@@ -1,4 +1,12 @@
 #ifndef HEADER
 #define HEADER
+
+#include <stdio.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+int print_argument(char specifier, va_list args, int count);
+int print_char(char c);
+int print_string(char* str);
+int print_integer(int params);
 #endif
