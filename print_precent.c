d7448eacac711855b8e7c845a6e5eae36c9134ff
azizios011 <azizhidri001@gmail.com>
2023-05-03 01:32:43 -1100

    print_percent

---
+++ print_precent.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * print_percent - check the code
+ * @args: argumment string
+ * Return: Always 0.
+*/
+int print_percent(va_list args)
+{
+	(void) args;
+
+	return (_putchar('%'));
+}
